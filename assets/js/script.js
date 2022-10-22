67db646e594f6528ba18f7258a5caa1140f7c0de
Daniel Diniz <ddanieldiniz@gmail.com>
2022-10-22 00:10:27 -0300

    script de carrosel e btn top criados.

--- assets/js/script.js
+++ assets/js/script.js
@@ -66,6 +66,42 @@ if(target.length) {
 
 //ANIMAÇÃO DE BLOCOS APARECENDO AO ROLAR SCROLL NA PAGINA -- FIM
 
+//ANIMAÇÃO CONTAGEM
+/* 
+const targetCount = document.querySelectorAll('[data-count]');
+
+function animeCount() {    
+    const windowTop = window.pageYOffset + ((window.innerHeight * 3) / 4);
+    targetCount.forEach(function(element){
+        setInterval(stepCount(element), 1000);
+        if((windowTop) > element.offsetTop) {
+            //stepCount(element);            
+        }
+    })
+}
+
+function stepCount(elemento) {
+    let el = elemento.querySelector('.contador');    
+    let valor = parseInt(elemento.dataset.count);
+    let c = parseInt(el.innerHTML);
+    console.log(c, valor);
+    if (c < valor){
+        c++;
+        el.innerHTML = c;        
+        //setInterval(stepCount(elemento), 5000);
+    }
+}
+
+animeCount();
+
+if(targetCount.length) {
+    window.addEventListener('scroll', function() {
+        animeCount();
+    })
+}
+*/
+//ANIMAÇÃO CONTAGEM --FIM
+
 //MODAL IMG
 
 let imagens = document.querySelectorAll('.small_img');
@@ -87,3 +123,46 @@ btClose.addEventListener('click', function(){
 });
 
 //MODAL IMG --FIM
+
+//Carousel de Imagens 
+
+const imgs = document.getElementById("img");
+const img = document.querySelectorAll("#img .slide");
+
+let idx = 0;
+
+function carrosel() {
+    idx++;
+    
+    if(idx > img.length - 1){
+        idx = 0;
+    }
+
+    imgs.style.transform = `translateX(${-idx * 100}vw)`;
+}
+
+setInterval(carrosel, 5000);
+
+//Carousel de Imagens -- FIM
+
+//BOTAO TOP
+
+const btn = document.getElementById("btnTop");
+
+btn.addEventListener("click", function(){
+    window.scrollTo(0,0);
+})
+
+document.addEventListener('scroll', ocultar);
+
+function ocultar(){
+    if(window.scrollY > 300){
+        btn.style.display = "flex"
+    } else {
+        btn.style.display = "none"
+    }
+}
+
+ocultar();
+
+//BOTAO TOP --FIM
